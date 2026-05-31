3e139464567a2ae6b1dfafdf5082df89ad7e951a
Ferhato <79317077+Ferhato@users.noreply.github.com>
2026-05-31 23:52:54 +0300

    Update main.c for better branch prediction
    We first tought that most of our userbase would be Cumali Gerigerigidengil so we had put the y line to come first, upon further inspectation we have realized that the most of our user base is NOT Cumali Gerigerigidengil and changed the order of branches. Also got rid of unnecessary "else" cases after if statements with return codes.

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main() {
   while(1) {
     printf("Are you Cumali Gerigerigidengil?\nanswer (y/n): ");
     scanf("%c",&ans);
+    if(ans=='n') {printf("you are NOT cumali gerigerigidemgil\n"); return 0;}
     if(ans=='y') {printf("you ARE Cumali Gerigerigidengil\n"); return 0;}
-    else if(ans=='n') {printf("you are NOT cumali gerigerigidemgil\n"); return 0;}
-    else printf("please write 'y' or 'n' as a single lowercase letter for us to determine if you are Cumali Gerigerigidengil or not!\n");
+    printf("please write 'y' or 'n' as a single lowercase letter for us to determine if you are Cumali Gerigerigidengil or not!\n");
   }
 }
